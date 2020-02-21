4556cc9d8fc7f8f7f721131a7e1217186d8bafd2
MuddledLuck <shrivastavaa278@gmail.com>
2020-02-21 20:08:08 +0530

    Adding some comments

--- dice.py
+++ dice.py
@@ -5,10 +5,10 @@ while True:
     dice = random.randrange(1, 7)
     print(f"And the Outcome is: {dice} \n")
     print("Continue or Quit: ", end = "")
-    user_input = input()
+    user_input = input()            # Get user input
     if user_input == "q" or user_input == "Q":
-        print("Thanks for rolling a dice :)")
+        print("Thanks for rolling a dice :)")           # Printing a thankyou massage
         break
     else:
-        print("\n \n")
+        print("\n \n")          #To make two line change
         continue
